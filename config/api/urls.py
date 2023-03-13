07795e8b69bcfcf2839041aab46a77f7efda40a5
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-03-13 15:32:54 +0330

    create user views in api app

--- config/api/urls.py
+++ config/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import ArticleList
+from .views import ArticleList, ArticleDetail, UserList, UserDetail
 
 app_name = 'api'
 
 urlpatterns = [
-  path("v1", ArticleList.as_view(), name='list'),
+  path("", ArticleList.as_view(), name='list'),
+  path("<int:pk>", ArticleDetail.as_view(), name='DETAIL'),
+  path("users/", UserList.as_view(), name='user-list'),
+  path("users/<int:pk>", UserDetail.as_view(), name='user-detail'),
+
 ]
--- config/api/views.py
+++ config/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import (ListCreateAPIView, RetrieveAPIView,
+                                     DestroyAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+from django.contrib.auth.models import User
 
-from .serializer import ArticleSerializer
+
+from .serializer import ArticleSerializer, UserSerializer
 # from ..blog.models import Article
 from blog.models import Article
 
@@ -10,3 +14,19 @@ from blog.models import Article
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+
+class ArticleDetail(DestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    #lookup_field = 'slug'
+
+
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
