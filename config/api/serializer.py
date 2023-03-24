58a26e2e7c2022684f110bd588cc277ffed29b8a
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-03-24 19:33:21 +0430

    add validation to serializer.py in Article class

--- config/api/serializer.py
+++ config/api/serializer.py
@@ -3,13 +3,25 @@ from blog.models import Article
 from django.contrib.auth import get_user_model
 
 
+
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = "__all__"
+
+    def validate_title(self, value):
+        filter_list = ["javascript", "c#", "golang"]
+
+        for i in filter_list:
+            if i in value:
+                raise serializers.ValidationError(f"Don't use this word: {value}")
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = "__all__"
+
+
